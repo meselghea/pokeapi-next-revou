7e9af32372445848506762644686e25873733e13
meselghea <122159476+meselghea@users.noreply.github.com>
2023-09-15 07:13:23 +0700

    feat: add pokemon card
    create router to pokemon card, styling

--- components/PokemonGrid.tsx
+++ components/PokemonGrid.tsx
@@ -1,3 +1,37 @@
+"use client"
+import { useState } from 'react';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
+import { PokemonCard } from './PokemonCard';
 export function PokemonGrid() {
-    
+const [ searchText, setSearchText ] = useState('');
+return (
+    <>
+    <div>
+        <h3 className='text-2xl py-6 text-center'>
+            Search For Your Pokemon!
+        </h3>
+        <div className='grid w-full max-w-sm items-center gap-1.5'>
+            <Label htmlFor='pokemonName'>
+                Pokemon Name
+            </Label>
+            <Input
+                type='text'
+                value={searchText}
+                id='pokemonName'
+                placeholder='Pikachu'
+                autoComplete='off'
+                onChange={(e) => setSearchText(e.target.value)}
+            />
+        </div>
+        <h3 className='text-3xl pt-12 pb-6 text-center'>Pokemon Collection</h3>
+    </div>
+
+    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+       <PokemonCard name='Pikachu' />
+       <PokemonCard name='Pikachu' />
+       <PokemonCard name='Pikachu' />
+    </div>
+    </>
+)
 }
